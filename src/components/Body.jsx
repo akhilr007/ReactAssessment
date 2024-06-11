da42407a83cf4315af6a793cb2aa27bfce614383
Akhil Rajan <rajanakhil07@gmail.com>
2024-06-11 18:30:33 +0530

    added welcome component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,9 @@
+import Welcome from "./Welcome";
+
 const Body = () => {
     return (
         <div className="w-[1366px] h-[1157px] border border-black">
-            <h3 className="text-[24px] leading-[30px] w-[496px] h-[32px] pl-[60px] pt-[23px] font-medium">
-                Welcome, Johan Paul
-            </h3>
+            <Welcome />
         </div>
     );
 };
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+const Welcome = () => {
+    return (
+        <div>
+            <h3 className="text-[24px] leading-[30px] w-[496px] h-[32px] pl-[60px] pt-[23px] font-medium">
+                Welcome, Johan Paul
+            </h3>
+        </div>
+    );
+};
+
+export default Welcome;
