f85426944838ffcbcf0f99498c4156ed4ea47c3a
Akhil Rajan <rajanakhil07@gmail.com>
2024-06-11 18:29:11 +0530

    added welcome note

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Body from "./components/Body";
 import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <div className="w-[1366px] mx-auto">
             <Navbar />
+            <Body />
         </div>
     );
 }
---
+++ src/components/Body.jsx
@@ -0,0 +1,11 @@
+const Body = () => {
+    return (
+        <div className="w-[1366px] h-[1157px] border border-black">
+            <h3 className="text-[24px] leading-[30px] w-[496px] h-[32px] pl-[60px] pt-[23px] font-medium">
+                Welcome, Johan Paul
+            </h3>
+        </div>
+    );
+};
+
+export default Body;
