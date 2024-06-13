f20938ade5b37dcc6f98c390bf060abcaf9908a4
Akhil Rajan <rajanakhil07@gmail.com>
2024-06-13 18:35:29 +0530

    added icon in navbar component when toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,18 +38,36 @@ const Navbar = () => {
                             d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
                         />
                     </svg>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="#4A4ED4"
-                        className="w-[10px] h-[10px] my-[18px] mr-[19px] "
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z"
-                            clipRule="evenodd"
-                        />
-                    </svg>
+
+                    {isPopupVisible ? (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="#4A4ED4"
+                            className="w-[10px] h-[10px] my-[18px] mr-[19px]"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m4.5 15.75 7.5-7.5 7.5 7.5"
+                            />
+                        </svg>
+                    ) : (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="#4A4ED4"
+                            className="w-[10px] h-[10px] my-[18px] mr-[19px] "
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    )}
                 </div>
             </nav>
 
